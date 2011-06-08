9834f31ebf05c736f12a516ecd520cfe442f166e
eXerigumo Clanjor (兰威举) <cjxgm@126.com>
2011-06-08 22:05:46 +0800

    2011年 06月 08日 星期三 22:05:45 CST

--- window manager/fvwm-2.6.1/modules/FvwmCommand/FvwmCommand.sh
+++
@@ -1,7 +0,0 @@
-# FvwmCommand.sh
-# Collection of fvwm builtin commands for FvwmCommand
-#
-alias FvwmCommand='/usr/local/bin/FvwmCommand'
-AM () { 
-	FvwmCommand "+ $*"
-}
--- window manager/pywm-0.1/examples/pywm/fltk_.pyx
+++
@@ -1,7 +0,0 @@
-#@+leo
-#@+node:0::@file src-python/fltk_.pyx
-#@+body
-#@@language python
-#@-body
-#@-node:0::@file src-python/fltk_.pyx
-#@-leo
