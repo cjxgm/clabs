f7fe54730a844ac5c6cf3a44ad5d5708c82034a3
eXerigumo Clanjor (哆啦比猫) <cjxgm@126.com>
2012-01-20 23:04:15 +0800

    modified:   canvas_svg/index.html modified:   canvas_svg/svg.js

--- canvas_svg/svg.js
+++ canvas_svg/svg.js
@@ -36,29 +36,9 @@ function bez(x1, y1, x2, y2, x3, y3, x4, y4)
 				0, 1, 0.01);
 }
 
-function calc()
-{
-}
-
 function draw()
 {
-	//sc.fillCircle(ball.phys.pos, ball.r, "#8ED6FF");
-	//sc.line(ground.p, ground.p2);
 	c.strokeStyle = "#f00";
 	draw_svg();
-}
-
-function timer()
-{
-	//sc.dur = sc.getTime() - sc.last_time;
-	sc.clear();
-	draw();
-	calc();
-	//sc.last_time += sc.dur;
-	//sc.time(timer, 30);
-}
-
-document.onload = function() {
-	sc.time(timer, 30);
 }
 
