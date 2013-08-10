ebb7302b5995f5f98d4209b2e6eff3dff90213c3
eXerigumo Clanjor (哆啦比猫) <cjxgm@126.com>
2013-08-10 22:16:04 +0800

    clean up

--- regexp/regexp.c
+++ regexp/regexp.c
@@ -1,55 +1,19 @@
 #include <stdio.h>
+#include <err.h>
 
-#define RE_ANY				0x0100
-#define RE_SPLIT			0x0101
-#define RE_JUMP				0x0102
-#define RE_GROUP_START		0x0200
-#define RE_GROUP_END		0x0201
-#define RE_MATCH			0xFFFF
+enum { RE_ANY = 0x0100, RE_SPLIT, RE_JUMP,
+		RE_GROUP_START, RE_GROUP_END, RE_MATCH };
 typedef struct RE_State
 {
 	unsigned short c;	// 0x0000-0x00FF: match literal; RE_* for specials
-	char g;				// greedy jump
-	char ng;			// non-greedy jump
-}
-RE_State;
-
-typedef struct RE_Match
-{
-	unsigned short s;	// start position
-	unsigned short e;	// end position
-}
-RE_Match;
-
+	char g, ng;			// greedy/non-greedy jump
+} RE_State;
+typedef struct RE_Match { unsigned short s, e; } RE_Match;
 typedef struct RE_Regexp
 {
 	RE_State state[256];
 	RE_Match match[256];
-}
-RE_Regexp;
-
-void RE_dump_state(RE_State * s, int i)
-{
-#define NR	printf("%3d  ", i)
-	switch (s->c) {
-		case RE_MATCH      : NR; printf(" M \n"); break;
-		case RE_ANY        : NR; printf(" A \n"); break;
-		case RE_SPLIT: NR; printf(" S %+4d%+4d\n", s->g, s->ng); break;
-		case RE_JUMP       : NR; printf(" J %+4d\n", s->g); break;
-		case RE_GROUP_START: NR; printf(" ( %4d\n", s->g); break;
-		case RE_GROUP_END  : NR; printf(" ) %4d\n", s->g); break;
-		default            : NR; printf("'%c'\n"   , s->c); break;
-	}
-#undef NR
-}
-
-void RE_dump_states(RE_State * s)
-{
-	for (int i=0; ; s++, i++) {
-		RE_dump_state(s, i);
-		if (s->c == RE_MATCH) break;
-	}
-}
+} RE_Regexp;
 
 RE_Regexp RE_compile(const char * regexp)
 {
@@ -65,8 +29,7 @@ RE_Regexp RE_compile(const char * regexp)
 		void prepend(unsigned short c, char g, char ng)
 		{
 			RE_State * s = s_tail++;
-			for (; s != s_head-1; s--)
-				*(s+1) = *s;
+			for (; s != s_head-1; s--) *(s+1) = *s;
 			*s_head = (RE_State){ c, g, ng };
 		}
 		for (; *regexp; regexp++) {
@@ -95,13 +58,8 @@ RE_Regexp RE_compile(const char * regexp)
 					s->g = s_tail-s;
 					regexp--;
 					break;
-				case '.':
-					s_head = s_tail;
-					append(RE_ANY, 0, 0);
-					break;
-				default:
-					s_head = s_tail;
-					append(*regexp, 0, 0);
+				case '.': s_head = s_tail; append(RE_ANY, 0, 0); break;
+				default : s_head = s_tail; append(*regexp, 0, 0);
 			}
 		}
 	}
@@ -130,30 +88,39 @@ int RE_match(RE_Regexp * re, const char * str)
 				default: if (*p != s->c) return 0;
 			}
 		}
-#if 0
-		for (;; s++) {
-			//printf(">> @%d(%x)\t[%d]%c(%2.2X)\n", s-re->state, s->c, p-str, *p, *p);
-			switch (s->c) {
-				case RE_SPLIT:
-					if (match(s+s-> g, p)) return 1;
-					if (match(s+s->ng, p)) return 1;
-					return 0;
-				case RE_JUMP: s += s->g-1; break;
-				case RE_MATCH: return 1;
-				case RE_GROUP_START: m[s->g].s = p-str; break;
-				case RE_GROUP_END  : m[s->g].e = p-str; break;
-				default: return 0;
-			}
-		}
-#endif
 		return 0;
 	}
 	return match(re->state, str);
 }
 
+
+
+
+void RE_dump_state(RE_State * s, int i)
+{
+#define NR	printf("%3d  ", i)
+	switch (s->c) {
+		case RE_MATCH      : NR; printf(" M \n"); break;
+		case RE_ANY        : NR; printf(" A \n"); break;
+		case RE_SPLIT: NR; printf(" S %+4d%+4d\n", s->g, s->ng); break;
+		case RE_JUMP       : NR; printf(" J %+4d\n", s->g); break;
+		case RE_GROUP_START: NR; printf(" ( %4d\n", s->g); break;
+		case RE_GROUP_END  : NR; printf(" ) %4d\n", s->g); break;
+		default            : NR; printf("'%c'\n"   , s->c); break;
+	}
+#undef NR
+}
+
+void RE_dump_states(RE_State * s)
+{
+	for (int i=0; ; s++, i++) {
+		RE_dump_state(s, i);
+		if (s->c == RE_MATCH) break;
+	}
+}
+
 int main()
 {
-	//RE_Regexp re = RE_compile("ab(c|def).*");
 	RE_Regexp re = RE_compile("a(((b|c)?d)+).*");
 	RE_dump_states(re.state);
 	{
